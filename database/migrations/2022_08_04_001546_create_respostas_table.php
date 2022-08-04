73854392aafb9777f10ee3ad89c77731d4ca8280
Luan S <luanfstack@gmail.com>
2022-08-03 21:23:44 -0300

    enquete model

---
+++ database/migrations/2022_08_04_001546_create_respostas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('respostas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('enquete_id')->constrained()->onDelete('cascade');
+            $table->string('resposta');
+            $table->integer('votes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('respostas');
+    }
+};
